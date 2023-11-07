706166b628e3b315bf15e3ae1851306da24fcb96
MysticBagel77902024 <147089054+MysticBagel77902024@users.noreply.github.com>
2023-11-07 15:32:26 -0700

    Update and rename cyber_pat_script_linux.bash to LinuxScript.sh

---
+++ LinuxScript.sh
@@ -0,0 +1,14 @@
+# below this, put a shebang, like this: "#! [file path]"
+echo "Script Output" | tee output.txt
+exec 1>& output.txt
+sudo apt update -y
+sudo apt install -y members
+sudo apt install -y net-tools
+sudo apt install -y locate
+cat /etc/passwd
+sudo ufw status
+sudo ufw enable
+members sudo
+sudo locate -i *.mp3
+sudo locate -i *.mp4
+sudo apt upgrade
